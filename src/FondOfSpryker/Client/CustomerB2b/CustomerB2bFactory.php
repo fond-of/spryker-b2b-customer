2288149489670d29490b551533371fc85e86e854
Markus Nörenberg <markus.noerenberg@fondofbags.com>
2019-07-29 12:02:26 +0200

    add return

--- src/FondOfSpryker/Client/CustomerB2b/CustomerB2bFactory.php
+++ src/FondOfSpryker/Client/CustomerB2b/CustomerB2bFactory.php
@@ -3,6 +3,7 @@
 namespace FondOfSpryker\Client\CustomerB2b;
 
 use FondOfSpryker\Client\CustomerB2b\Zed\CustomerB2bStub;
+use FondOfSpryker\Client\CustomerB2b\Zed\CustomerB2bStubInterface;
 use Spryker\Client\Customer\CustomerFactory as SprykerCustomerFactory;
 
 class CustomerB2bFactory extends SprykerCustomerFactory
@@ -11,7 +12,7 @@ class CustomerB2bFactory extends SprykerCustomerFactory
      * @throws
      * @return \FondOfSpryker\Client\CustomerB2b\Zed\CustomerB2bStubInterface
      */
-    public function createZedCustomerStub()
+    public function createZedCustomerStub(): CustomerB2bStubInterface
     {
         return new CustomerB2bStub($this->getProvidedDependency(CustomerB2bDependencyProvider::SERVICE_ZED));
     }
